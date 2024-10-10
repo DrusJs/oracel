e72d22cc21d5f14d770699a005026679f5db558b
drusjs <magnojoky962@gmail.com>
2024-10-10 18:19:47 +0300

    fix

--- src/components/pages/SignInPage.tsx
+++ src/components/pages/SignInPage.tsx
@@ -48,7 +48,7 @@ export default function SignInPage() {
                     <p className="font-body md:text-2xl text-base font-medium md:text-center text-gray-secondary mt-4 max-w-[700px]">Don’t worry, resetting your passward is easy!
                     Enter the email associated with your account and we’ll send an email with instructions to reset your passward.</p>
                     <form className="flex flex-col w-full lg:max-w-2xl md:mt-12 mt-8">                        
-                        <input type="email" name="email-input" id="email-input" className="outline-transparent w-full font-display md:text-[22px] text-base text-white py-6 md:px-10 px-6 bg-input rounded-full mt-10" placeholder="Your e-mail" />
+                        <input type="email" name="email-input" id="email-input" className="outline-transparent w-full font-display md:text-[22px] text-base text-white py-6 md:px-10 px-6 bg-input-dark rounded-full mt-10" placeholder="Your e-mail" />
                         <button className="w-full bg-green text-dark md:text-2xl text-lg font-display md:py-6 py-6 mt-10 flex items-center justify-center font-bold rounded-full uppercase">
                             SEND A CODE 
                         </button>
